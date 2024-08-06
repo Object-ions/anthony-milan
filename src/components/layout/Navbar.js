7ca3a8f8da7fe77ea55a753447b85d3a9f16f8dc
Object-ions <moshikoatia@gmail.com>
2024-08-05 22:21:56 -0700

    Add navbar and ite style

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,27 @@
+import '../../assets/styles/navbar.scss';
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#plans">Plans & Pricing</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <ul>
+        <li>Instagram</li>
+        <li>@anthony_milian</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
